5b1fde883022a3636a1b9e079393c55405daa193
Jon Warbrick <jw35@cam.ac.uk>
2019-07-08 15:47:21 +0100

    Add cheetsheet

--- Makefile
+++ Makefile
@@ -1,15 +1,23 @@
-default: dist/tunebook.abc dist/tunebook.pdf dist/tunebook-whistle.pdf
+default: dist/tunebook.abc dist/tunebook.pdf dist/tunebook-whistle.pdf dist/cheatsheet.pdf
 
 dist/tunebook.abc : header.abc abc/*.abc
 	mkdir -p dist
 	(echo '%abc-2.1'; \
-		cat header.abc; \
-		echo; \
-		for f in `ls abc/*.abc`; do (grep -v '%abc-2.1' "$${f}"; echo;) done) > $@
+	 cat header.abc; \
+	 echo; echo; \
+	 for f in `ls abc/*.abc`; do (grep -v '%abc-2.1' "$${f}"; echo;) done \
+	) > $@
 	mac2unix $@
 
 dist/tunebook.pdf : dist/tunebook.abc
+	mkdir -p dist
 	abcm2ps $< -i -O - | ps2pdf - $@
 
 dist/tunebook-whistle.pdf : dist/tunebook.abc
+	mkdir -p dist
 	abcm2ps $< -i -F flute.fmt -T1 -O - | ps2pdf - $@
+
+dist/cheatsheet.pdf : abc/*.abc bin/make_cheatsheet.py
+	(echo '%abc-2.1'; \
+  	 for f in `ls abc/*.abc`; do (grep -v '%abc-2.1' "$${f}"; echo;) done \
+  	) | bin/make_cheatsheet.py | abcm2ps - -i -O - | ps2pdf - $@
---
+++ bin/make_cheatsheet.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+'''
+Generate a 'cheatsheet' (or 'Incipit') containing just the first few bars of each
+tune. This script is good enough to extract information from the tunes
+in this repository, but doesn't include a complete ABC parser and is likely
+to choke on entirely legal ABC files that happen to include features not
+used here.
+'''
+
+import fileinput
+import re
+
+from fractions import Fraction
+
+# "borrowed' from EasyABC (https://sourceforge.net/projects/easyabc/)"
+note_pattern = re.compile(r"(?P<note>([_=^]?[A-Ga-gxz](,+|'+)?))(?P<length>\d{0,3}(?:/\d{0,3})*)(?P<dot>\.*)(?P<broken>[><]?)")
+bar_pattern = re.compile(r'(?P<bar>.*?\|)')
+
+
+def get_default_len(abc):
+    m = re.search(r'(?m)^L: *(\d+)/(\d+)', abc)
+    if m:
+        return Fraction(int(m.group(1)), int(m.group(2)))
+    else:
+        return Fraction(1, 8)
+
+
+def get_metre(abc):
+    m = re.search(r'(?m)^M: *(\d+)/(\d+)', abc)
+    if m:
+        return Fraction(int(m.group(1)), int(m.group(2)))
+    else:
+        return Fraction(4, 4)
+
+
+def bar_length(notes, default_length):
+
+    total_length = 0
+
+    for match in note_pattern.finditer(re.sub(r'".*?"', '', notes)):
+
+        #print(match.group('note'), match.group('length'), match.group('dot'))
+
+        length = match.group('length')
+        multiplier = Fraction(1)
+        dividend = length.split('/')[0]
+        if dividend:
+            multiplier = multiplier * Fraction(int(dividend))
+        for divmatch in re.finditer(r'/(\d*)', length):
+            divisor = divmatch.group(1)
+            if divisor:
+                divisor = int(divisor)
+            else:
+                divisor = 2
+            multiplier = multiplier / Fraction(divisor)
+        for dot in match.group('dot'):
+            multiplier = multiplier * Fraction(3, 2)
+
+        #print(multiplier * default_length)
+
+        total_length += multiplier * default_length
+
+    return total_length
+
+
+length_so_far = 0
+metre = Fraction(4, 4)
+default_length = Fraction(1, 16)
+
+
+for line in fileinput.input():
+
+    if re.match('X:', line):
+        length_so_far = 0
+        metre = Fraction(4, 4)
+        default_length = Fraction(1, 16)
+    elif re.match('L:', line):
+        default_length = get_default_len(line)
+    elif re.match('M:', line):
+        metre = get_metre(line)
+
+    # Blank lines
+    if re.fullmatch(r'\s*', line):
+        print(line, end='')
+    # Headers and comments
+    elif re.match('([A-Za-z]:)|%', line):
+        print(line, end='')
+    # Notes
+    elif length_so_far < 2*metre:
+
+        # Dump leading bar marker
+        line = re.sub(r'\s*\|:?\s*', '', line, count=1)
+
+        for match in bar_pattern.finditer(line):
+            bar = match.group('bar')
+            #print('Got bar', bar)
+            length = bar_length(bar, default_length)
+            #print('Length', length)
+
+            print(bar, end='')
+            length_so_far += length
+            if length_so_far >= 2 * metre:
+                break
+
+        print()
